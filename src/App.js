2f0fe2e69e3aa494e45675aac560d4adc2d9bb04
ryrye <ryan@entra.ca>
2021-03-31 09:38:01 -0700

    Frontend working to walk around world, display tiles, build basic player

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { PlayerBuilder } from "./PlayerBuilder";
+import { Xoxaria } from "./Xoxaria";
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 function App() {
+
+  const [player, setPlayer] = useState(
+    cookies.get('player') || {
+      name: ''
+    }
+  ); 
+
+  const updatePlayer = player => {
+    setPlayer(player)
+    cookies.set('player',player)
+  }
+
+  const resetPlayer = () => {
+    player.name = ""     
+    setPlayer({ ...player})
+    cookies.set('player',player)
+  }
+
+  // cookies.remove('player')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header 
+        className="App-header"
+        key = {player.name}      
+      > 
+      { player.name }
+        { ! player.name && 
+          <PlayerBuilder 
+            player={player}
+            setPlayer={updatePlayer}
+          />
+        }
+        {player.name && (   
+          <Xoxaria           
+            player = {player }           
+            resetPlayer = {resetPlayer} 
+          />                     
+        )}
       </header>
     </div>
   );
